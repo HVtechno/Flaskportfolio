a394c6c889d5a3da7eba681d7da7e5e5ac737187
HVtechno <htechno0786@gmail.com>
2024-02-08 16:01:26 +0100

    app_updated

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 from keras.models import load_model
-model = load_model('static/models/model.h5')
 import json
 import random
 from flask import Flask, render_template, request, jsonify
@@ -27,55 +26,52 @@ RECEIVER = os.getenv('SMTP_RECEIVER')
 app = Flask(__name__)
 CORS(app)
 
-intents = json.loads(open('static/intents/data.json').read())
-words = pickle.load(open('static/models/texts.pkl','rb'))
-classes = pickle.load(open('static/models/labels.pkl','rb'))
+# Load resources
+with open('static/intents/data.json') as file:
+    intents = json.load(file)
+
+with open('static/models/texts.pkl', 'rb') as file:
+    words = pickle.load(file)
+
+with open('static/models/labels.pkl', 'rb') as file:
+    classes = pickle.load(file)
+
+# Load the model
+model = load_model('static/models/model.h5')
 
 def clean_up_sentence(sentence):
-    # tokenize the pattern - split words into array
-    sentence_words = nltk.word_tokenize(sentence)
-    # stem each word - create short form for word
-    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
-    return sentence_words
-# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
-def bow(sentence, words, show_details=True):
-    # tokenize the pattern
-    sentence_words = clean_up_sentence(sentence)
-    # bag of words - matrix of N words, vocabulary matrix
-    bag = [0]*len(words)  
-    for s in sentence_words:
-        for i,w in enumerate(words):
-            if w == s: 
-                # assign 1 if current word is in the vocabulary position
-                bag[i] = 1
-                if show_details:
-                    print ("found in bag: %s" % w)
-    return(np.array(bag))
-def predict_class(sentence, model):
-    # filter out predictions below a threshold
-    p = bow(sentence, words,show_details=False)
-    res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
-    # sort by strength of probability
-    results.sort(key=lambda x: x[1], reverse=True)
-    return_list = []
-    for r in results:
-        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
-    return return_list
+    return [lemmatizer.lemmatize(word.lower()) for word in nltk.word_tokenize(sentence)]
+
+def bow(sentence, words):
+    return np.array([1 if word in sentence else 0 for word in words])
+
+def predict_class(sentences):
+    # Clean up sentences
+    cleaned_sentences = [clean_up_sentence(sentence) for sentence in sentences]
+    
+    # Convert sentences to bags of words
+    bows = [bow(sentence, words) for sentence in cleaned_sentences]
+    
+    # Predict classes for all sentences
+    predictions = model.predict(np.array(bows))
+    
+    # Process predictions for each sentence
+    return [process_prediction(prediction) for prediction in predictions]
+
+def process_prediction(prediction):
+    # Filter out predictions below threshold
+    indexes = np.where(prediction > 0.25)[0]
+    return [(i, prediction[i]) for i in indexes]
+
 def getResponse(ints, intents_json):
-    tag = ints[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
-    return result
+    tag = classes[ints[0][0]]
+    for intent in intents_json['intents']:
+        if intent['tag'] == tag:
+            return random.choice(intent['responses'])
 
 def chatbot_response(msg):
-    ints = predict_class(msg, model)
-    res = getResponse(ints, intents)
-    return res
+    ints = predict_class([msg])[0]
+    return getResponse(ints, intents)
 
 @app.route('/')
 def index():
@@ -133,6 +129,6 @@ def send_confirmation_email(user_data):
 
 if __name__ == '__main__':
     options = {
-        'request_timeout': 120  # Set the timeout to 60 seconds
+        'request_timeout': 120  # Set the timeout to 120 seconds
     }
     app.run(debug=True, threaded=True, host='0.0.0.0', port=5000, **options)
